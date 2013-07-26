a9013fca7ff51179d46709b8699cca01dee716f7
kamereon <imperator@boxhamsters.net>
2013-07-26 21:03:40 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -148,7 +148,7 @@
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
 										width: 1000,
 										height: 217,
-										renderer: 'line',
+										renderer: 'area',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										padding: {
