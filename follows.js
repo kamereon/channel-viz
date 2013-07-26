d77b572be4f9e986a1b211b1b86a9c15ccbaabf8
kamereon <imperator@boxhamsters.net>
2013-07-26 22:46:12 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -17,7 +17,7 @@
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '1week', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 900, // Default interval for data to be displayed (in seconds)
-		dataColor	= '000000', // CSS HEX value of color to represent data (omit leading #)
+		dataColor	= '770000', // CSS HEX value of color to represent data (omit leading #)
 		hideForm	= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
@@ -146,8 +146,8 @@
 						 			// Build Graph
 									var graph = new Rickshaw.Graph( {
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
-										width: 300,
-										height: 600,
+										width: 1600,
+										height: 300,
 										renderer: 'line',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
